37cbb6f36bd4ffa2f387d52b7c2c34f96ef9ea34
GkhKaya <gkhkaya00@gmail.com>
2023-06-03 23:56:51 +0300

    Created Landmarks and Finished Creating and Combininng Views Part

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Gokhan Kaya on 3.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MapView()
+        }
+    }
+}
